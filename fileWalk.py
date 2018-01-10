37c99228c9aee8deb4d444acab92e38a58c10c5b
cabrody <crb99@aol.com>
2018-01-09 18:56:45 -0800

    Add files via upload

---
+++ fileWalk.py
@@ -0,0 +1,38 @@
+import os
+import fnmatch
+import shutil
+
+folderCount = -1
+numOfSubs = 0
+nextOne = 0
+int_list = []
+
+for dirName, subdirList, fileList in os.walk("."):
+	if dirName is '.':
+		dirList = subdirList
+		for sub in dirList:
+			with open("list.txt", "a") as myfile:
+				myfile.write(sub+'\r\n')
+		with open("list.txt", "a") as myfile:
+			myfile.write('\r\n')
+		pass
+	else:
+		try:
+			new_file = os.path.join(dirList[folderCount] + "/", dirName.split('_')[6])
+			shutil.move(dirName, new_file)
+			numOfSubs += 1
+		except IndexError:
+			folderCount += 1
+			if nextOne == 0:
+				nextOne = 1
+				pass
+			else:
+				int_list.append(numOfSubs)
+			numOfSubs = 0
+			pass
+
+int_list.append(numOfSubs)
+for num in int_list:
+	with open("list.txt", "a") as myfile:
+		myfile.write(str(num))
+		myfile.write("\r\n")
